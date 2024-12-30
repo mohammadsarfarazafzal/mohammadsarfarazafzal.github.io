66cbe7768974ee3860da0750d64c865171be7dd9
Mohammad Sarfaraz Afzal <sanjum96786@gmail.com>
2024-12-30 21:31:48 +0530

    Changing base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mohammadsarfarazafzal.github.io/',
+  base: '/',
   server: {
     host: true
   }
